842ce70cade85b7d35b332219abed72dbc76041f
hungkinang <nvh234205@gmail.com>
2026-06-01 22:14:15 +0700

    upd

--- src/main/java/edu/bookingtour/controller/AuthController.java
+++ src/main/java/edu/bookingtour/controller/AuthController.java
@@ -93,7 +93,7 @@ public class AuthController {
 
         // Kiểm tra vai trò và chuyển hướng tương ứng
         if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
-            return "redirect:/admin/user";
+            return "redirect:/admin";
         } else {
             return "redirect:/";
         }
